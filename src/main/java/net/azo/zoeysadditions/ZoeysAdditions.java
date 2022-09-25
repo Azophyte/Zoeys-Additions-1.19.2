96a2651587ca40682bd1ae3f091097826c3ab5e3
Azo <Azophyte@gmail.com>
2022-09-25 23:49:00 +0100

    Removed junk comments

---
+++ src/main/java/net/azo/zoeysadditions/ZoeysAdditions.java
@@ -0,0 +1,18 @@
+package net.azo.zoeysadditions;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZoeysAdditions implements ModInitializer {
+	public static final String MOD_ID = "zoeysadditions";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/azo/zoeysadditions/ZoeysAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.azo.zoeysadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZoeysAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
