d817a9e3d064c0bea68e41a987a818238f753add
Azo <Azophyte@gmail.com>
2023-03-22 20:04:10 +0000

    - Added

---
+++ src/main/java/net/azo/zoeysadditions/block/entity/TownCrier.java
@@ -0,0 +1,9 @@
+package net.azo.zoeysadditions.block.entity;
+
+import net.minecraft.block.Block;
+
+public class TownCrier extends Block {
+    public TownCrier(Settings settings) {
+        super(settings);
+    }
+}
