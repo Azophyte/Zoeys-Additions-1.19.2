e2df1257ac6685538c83822ceaab349b7e78ace4
Azo <Azophyte@gmail.com>
2023-04-02 03:00:52 +0100

    - Deleted

--- src/main/java/net/azo/zoeysadditions/block/entity/TownCrier.java
+++
@@ -1,9 +0,0 @@
-package net.azo.zoeysadditions.block.entity;
-
-import net.minecraft.block.Block;
-
-public class TownCrier extends Block {
-    public TownCrier(Settings settings) {
-        super(settings);
-    }
-}
