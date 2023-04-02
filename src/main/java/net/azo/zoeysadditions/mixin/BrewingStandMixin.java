ab07865af32603c3e75704563308a34d06e55e88
Azo <Azophyte@gmail.com>
2023-04-02 02:53:00 +0100

    - Tried to delete, couldn't figure out how, brought it back with shame

---
+++ src/main/java/net/azo/zoeysadditions/mixin/BrewingStandMixin.java
@@ -0,0 +1,9 @@
+package net.azo.zoeysadditions.mixin;
+
+
+
+
+//The point of this class is to allow the brewing stand entity to brew items that aren't potions.
+//That's literally it.
+public class BrewingStandMixin {
+}
