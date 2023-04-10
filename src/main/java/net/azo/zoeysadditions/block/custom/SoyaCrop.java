b9a533962d5b45945127e6eca429f442dbeb5523
Azo <Azophyte@gmail.com>
2023-04-10 05:25:37 +0100

    - updated the bounding box

--- src/main/java/net/azo/zoeysadditions/block/custom/SoyaCrop.java
+++ src/main/java/net/azo/zoeysadditions/block/custom/SoyaCrop.java
@@ -1,21 +1,17 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package net.azo.zoeysadditions.block.custom;
 
 import net.azo.zoeysadditions.item.ModItems;
 import net.minecraft.block.*;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
 public class SoyaCrop extends CropBlock {
-    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 2.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 3.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 5.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 7.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0), Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 9.0, 16.0)};
-
+    private static final VoxelShape SHAPE_0 = Block.createCuboidShape(6.0, 0.0, 6.0, 10.0, 4.0, 10.0);
+    private static final VoxelShape SHAPE_1 = Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 8.0, 11.0);
+    private static final VoxelShape SHAPE_2 = Block.createCuboidShape(4.0, 0.0, 4.0, 12.0, 12.0, 12.0);
+    private static final VoxelShape SHAPE_3 = Block.createCuboidShape(3.0, 0.0, 3.0, 13.0, 14.0, 13.0);
     public SoyaCrop(AbstractBlock.Settings settings) {
         super(settings);
     }
@@ -25,6 +21,11 @@ public class SoyaCrop extends CropBlock {
     }
 
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return AGE_TO_SHAPE[(Integer)state.get(this.getAgeProperty())];
+        return switch (state.get(AGE) / 2) {
+            case 0 -> SHAPE_0;
+            case 1 -> SHAPE_1;
+            case 2 -> SHAPE_2;
+            default -> SHAPE_3;
+        };
     }
 }
